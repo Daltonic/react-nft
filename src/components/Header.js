86639249adf78b608a5d3c4faecb9ae07f2ff1dd
Daltonic <darlingtongospel@gmail.com>
2021-12-21 15:44:48 +0100

    done with the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import './Header.css'
 import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+import themeSwitchIcon from '../assets/header/theme-switch.png'
 
 const Header = () => {
   return (
     <div className="header">
       <div className="punkLogoContainer">
-        <img src={punkLogo} className="punkLogo" alt="" />
+        <img src={punkLogo} className="punkLogo" alt="logo" />
       </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="search-icon" />
+        </div>
+
+        <input
+          type="search"
+          className="searchInput"
+          placeholder="Collection, item, user..."
+        />
+      </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+          <div className="themeSwitchContainer">
+              <img src={themeSwitchIcon} alt="theme-switch-icon" />
+          </div>
+      </div>
+
+      <div className="loginButton">GET IN</div>
     </div>
   )
 }
